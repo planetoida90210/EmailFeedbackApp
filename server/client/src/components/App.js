1eaa35725ba96fae1379b6be6d9c22f024630e9e
galeria koloru <galeriakoloru90210@gmail.com>
2020-12-01 23:51:56 +0100

    add routes to site

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/surveys' component={Dashboard} />
+          <Route path='/surveys/new' component={SurveyNew} />
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
