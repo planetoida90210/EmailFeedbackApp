4118cb35c452288fbf0f95b1911c709b562c0ef1
galeria koloru <galeriakoloru90210@gmail.com>
2020-12-02 23:27:03 +0100

    add materialize-css

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const Header = () => <h2>Header</h2>;
+
+import Header from './Header'
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -11,6 +12,7 @@ const App = () => {
     <div>
       <Router>
         <div>
+          <Header />
           <Route exact path='/' component={Landing} />
           <Route exact path='/surveys' component={Dashboard} />
           <Route path='/surveys/new' component={SurveyNew} />
